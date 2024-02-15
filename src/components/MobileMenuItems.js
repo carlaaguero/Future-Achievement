c0092c8a91c3dd5724953fc2b82f734eba9bf81d
carlaaguero <c.agueromiranda@gmail.com>
2024-02-15 11:38:56 +0100

    Updating new elements to the Navbar component

---
+++ src/components/MobileMenuItems.js
@@ -0,0 +1,38 @@
+export const MobileMenuItems = [
+    {
+        title : "Home",
+        url : "/",
+        cName : "nav-links",
+        icon : "fa-solid fa-house-user"
+    },
+    {
+        title : "Services",
+        url : "/services",
+        cName : "nav-links",
+        icon : "fa-solid fa-briefcase"
+    },
+    {
+        title : "Clients",
+        url : "/clients",
+        cName : "nav-links",
+        icon : "fa-solid fa-handshake"
+    },
+    {
+        title : "Contact",
+        url : "/contact",
+        cName : "nav-links",
+        icon : "fa-solid fa-address-book"
+    },
+    {
+        title: "Switch to Arabic",
+        url: "https://futureachievement.netlify.app/",
+        cName: "nav-links",
+        icon: "fa-solid fa-globe"
+    },
+    {
+        title : "",
+        url : "tel://00966550870588",
+        cName : "nav-links-mobile",
+        icon : "fa-solid fa-phone"
+    },
+]
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,73 @@
-import { Component } from "react";
-import "./NavbarStyles.css";
-import { MenuItems } from "./MenuItems"
+import React, { Component } from "react"
+import "./NavbarStyles.css"
 import logo from "../assets/logo-1.png"
 import { Link } from "react-router-dom"
+import { MenuItems as DesktopMenuItems } from "./MenuItems"
+import { MobileMenuItems } from "./MobileMenuItems"
 
 class Navbar extends Component {
-    state = { clicked: false };
-    handleClick = () =>{
-        this.setState({ clicked: !this.state.clicked})
+    state = {
+        clicked: false,
+        isMobile: false
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+        this.handleResize();
     }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.handleResize);
+    }
+
+    handleResize = () => {
+        const isMobile = window.innerWidth < 950;
+        this.setState({ isMobile });
+    };
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    };
+
     render() {
+        const { isMobile, clicked } = this.state;
+        const MenuItems = isMobile ? MobileMenuItems : DesktopMenuItems;
+
         return (
             <nav className="NavbarItems">
-                 <Link to="/"><img src={logo} className="logo" alt="Future Archievement Logo" /></Link>
-
-                <div className="menu-icons" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                <div className="logo-container">
+                    <a href="/">
+                        <img src={logo} className="logo" alt="Future Archievement Logo" />
+                    </a>
+                    {!isMobile && (
+                        <a href="https://futureachievement.netlify.app/" className="language-button">
+                            <i className="fa-solid fa-globe"></i>
+                            AR
+                        </a>
+                    )}
                 </div>
-                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-                    {MenuItems.map((item, index) =>{
-                        return(
-                            <li key={index}>
-                       <Link className={item.cName} to={item.url}>
-                       <i className={item.icon}></i>{item.title}
-                       </Link> 
-                    </li>
-
-                        );
-                    })}
-                    <a href="tel:+966550870588"><button> <i class="fa-solid fa-phone"></i></button></a>
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+                <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuItems.map((item, index) => (
+                        <li key={index}>
+                            <Link className={item.cName} to={item.url}>
+                                <i className={item.icon}></i>
+                                {item.title}
+                            </Link>
+                        </li>
+                    ))}
+                    {!isMobile && (
+                        <li>
+                            <a href="tel:+966550870588" className="phone-button">
+                                <i className="fa-solid fa-phone"></i>
+                            </a>
+                        </li>
+                    )}
                 </ul>
             </nav>
         );
-
     }
 }
 
